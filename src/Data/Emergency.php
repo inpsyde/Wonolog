e86d7690ef6949fe651be1bec602890537f9774b
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-10-18 09:53:40 +0200

    Remove typo from packahe header

--- src/Data/Emergency.php
+++ src/Data/Emergency.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of the Inpsyde Inpsyde wonolog package.
+ * This file is part of the Inpsyde wonolog package.
  *
  * (c) Inpsyde GmbH
  *
--- tests/boot.php
+++ tests/boot.php
@@ -1,6 +1,6 @@
 <?php # -*- coding: utf-8 -*-
 /*
- * This file is part of the Inpsyde Inpsyde wonolog package.
+ * This file is part of the Inpsyde wonolog package.
  *
  * (c) Inpsyde GmbH
  *
