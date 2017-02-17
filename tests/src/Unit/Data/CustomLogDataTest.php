1f2ecceb9486dc24c56af30b57f2a8e3b497279a
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2017-02-17 13:01:01 +0100

    Removed VariableLevelLog
    Ssee #11 Fixes #13

--- tests/src/Unit/Data/CustomLogDataTest.php
+++ tests/src/Unit/Data/CustomLogDataTest.php
@@ -19,7 +19,6 @@ use Inpsyde\Wonolog\Data\Error;
 use Inpsyde\Wonolog\Data\Info;
 use Inpsyde\Wonolog\Data\LogDataInterface;
 use Inpsyde\Wonolog\Data\Notice;
-use Inpsyde\Wonolog\Data\VariableLevelLog;
 use Inpsyde\Wonolog\Data\Warning;
 use Inpsyde\Wonolog\Tests\TestCase;
 use Monolog\Logger;
@@ -31,61 +30,33 @@ use Monolog\Logger;
  */
 class CustomLogDataTest extends TestCase {
 
-	public function test_levels() {
+	/**
+	 * @dataProvider log_levels
+	 *
+	 * @param int              $expected_level
+	 * @param LogDataInterface $log
+	 */
+	public function test_levels( $expected_level, LogDataInterface $log ) {
 
-		/** @var LogDataInterface[] $data */
-		$data = [
-			Logger::ALERT     => new Alert( 'test', Channels::DEBUG ),
-			Logger::CRITICAL  => new Critical( 'test', Channels::DEBUG ),
-			Logger::DEBUG     => new Debug( 'test', Channels::DEBUG ),
-			Logger::EMERGENCY => new Emergency( 'test', Channels::DEBUG ),
-			Logger::ERROR     => new Error( 'test', Channels::DEBUG ),
-			Logger::INFO      => new Info( 'test', Channels::DEBUG ),
-			Logger::NOTICE    => new Notice( 'test', Channels::DEBUG ),
-			Logger::WARNING   => new Warning( 'test', Channels::DEBUG ),
+		self::assertSame( Channels::DEBUG, $log->channel() );
+		self::assertSame( 'test', $log->message() );
+		self::assertSame( [], $log->context() );
+		self::assertSame( $expected_level, $log->level() );
+
+	}
+
+	public function log_levels() {
+
+		return [
+			[ Logger::ALERT, new Alert( 'test', Channels::DEBUG ) ],
+			[ Logger::CRITICAL, new Critical( 'test', Channels::DEBUG ) ],
+			[ Logger::DEBUG, new Debug( 'test', Channels::DEBUG ) ],
+			[ Logger::EMERGENCY, new Emergency( 'test', Channels::DEBUG ) ],
+			[ Logger::ERROR, new Error( 'test', Channels::DEBUG ) ],
+			[ Logger::INFO, new Info( 'test', Channels::DEBUG ) ],
+			[ Logger::NOTICE, new Notice( 'test', Channels::DEBUG ) ],
+			[ Logger::WARNING, new Warning( 'test', Channels::DEBUG ) ],
 		];
-
-		foreach ( $data as $expected_level => $log ) {
-			self::assertSame( Channels::DEBUG, $log->channel() );
-			self::assertSame( 'test', $log->message() );
-			self::assertSame( [], $log->context() );
-			self::assertSame( $expected_level, $log->level() );
-		}
-
-	}
-
-	public function test_variable_level() {
-
-		$c = 0;
-
-		$log = VariableLevelLog::instance( 'test', Channels::SECURITY, [ 'foo' ] )
-			->with_level_callback(
-				function () use ( &$c ) {
-
-					$c ++;
-
-					return $c;
-				}
-			);
-
-		self::assertSame( Channels::SECURITY, $log->channel() );
-		self::assertSame( 'test', $log->message() );
-		self::assertSame( [ 'foo' ], $log->context() );
-
-		self::assertSame( 1, $log->level() );
-		self::assertSame( 2, $log->level() );
-		self::assertSame( 3, $log->level() );
-	}
-
-	public function test_variable_level_no_callback() {
-
-		$log = VariableLevelLog::instance( 'test', Channels::SECURITY, [ 'foo' ] );
-
-		self::assertSame( Channels::SECURITY, $log->channel() );
-		self::assertSame( 'test', $log->message() );
-		self::assertSame( [ 'foo' ], $log->context() );
-
-		self::assertSame( Logger::DEBUG, $log->level() );
 	}
 
 }
