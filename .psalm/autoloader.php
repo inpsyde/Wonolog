9f5aa717366b847a4a4bf2daee6e30dd9ea6a2d9
cleucht <c.leucht@inpsyde.com>
2020-05-29 10:24:17 +0200

    init commit of 2. Q&A.
    added psalm, inpsyde coding standards 1.x, update phpunit to ^8, update min php to 7.2, update monolog to 2.x, add github actions.

---
+++ .psalm/autoloader.php
@@ -0,0 +1,17 @@
+<?php // phpcs:disable
+if (defined('ABSPATH')) {
+    return;
+}
+
+define('ABSPATH', dirname(__DIR__).'/vendor/wordpress/wordpress/');
+define('WPINC', 'wp-includes');
+define('OBJECT', 'OBJECT');
+
+$wpIncPath = ABSPATH."wp-includes/";
+
+require dirname(__DIR__)."/inc/functions.php";
+require "{$wpIncPath}class-wp-post.php";
+require "{$wpIncPath}class-wp-error.php";
+require "{$wpIncPath}load.php";
+require "{$wpIncPath}plugin.php";
+require "{$wpIncPath}functions.php";
