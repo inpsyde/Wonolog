8cb626b2ffc3080e36c43d8ca76780f41e44663a
Anas Kashalo <anas.kashalo@gmail.com>
2025-04-07 10:39:27 +0300

    Remove class docs

--- src/Configurator.php
+++ src/Configurator.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of the Wonolog package.
- *
- * (c) Inpsyde GmbH
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 namespace Inpsyde\Wonolog;
